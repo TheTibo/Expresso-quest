5276fbdf50b801c49967d5ae50b79ae45fba988d
Thibault Zhou <thetibo1@gmail.com>
2024-02-13 12:02:58 +0100

    quest1DONE

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,43 @@
+
+const database = require("../../database");
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+
+    .then(([users]) => {
+      res.json(users); // use res.json instead of console.log
+    })
+
+    .catch((err) => {
+      console.error(err);
+
+      res.sendStatus(500);
+    });
+};
+
+const getUsersbyId = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id =  ?", [id])
+
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users); // use res.json instead of console.log
+      } else {
+        res.sendStatus(404);
+      }
+    })
+
+    .catch((err) => {
+      console.error(err);
+
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUsersbyId,
+};
